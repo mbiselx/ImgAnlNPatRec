c7ccfc28c754c75c751a3110b62ef66682fd3c5a
mbiselx <michael.biselx@epfl.ch>
2022-05-27 20:52:17 +0200

    it kind of works-ish but not very well

--- project/test.py
+++ project/test.py
@@ -30,27 +30,30 @@ def get_fourier_descriptors(contour, nb_descr=2, descr_idx=[], make_contour=Fals
     else :                                                                      # otherwise take the nb_descr lowest frequencies
         descr_idx = np.arange(-np.floor(nb_descr/2), np.ceil(nb_descr/2)+1, dtype=int)
 
-    # step 3 : extract the location and relevant descriptors
-    location = np.array([fft_cont[0].real, fft_cont[0].imag])/fft_cont.shape[0] # get the location of the center
+    # step 3 : extract the location, rotation and relevant descriptors
+    location    = np.array([fft_cont[0].real, fft_cont[0].imag])/fft_cont.shape[0] # get the location of the center
+    # rotation    = np.exp(-1j*(np.angle(fft_cont[1]) + np.angle(fft_cont[0])))                             # rotation is given by the phase of the non-DC component
+    rotation    = np.exp(-1j*np.angle(fft_cont[1]))
+    # rotation    = 1
     descriptors = fft_cont[descr_idx[descr_idx!=0]]                             # remove the idx '0' (location)
 
     # step 4 : normalize descriptors onto the unit ball, but keep their relative phases
-    # descriptors = np.abs(descriptors) / np.linalg.norm(descriptors)                     # normalize over descriptors
+    # descriptors = descriptors * rotation                                        #  only correct for initial phase
+    # descriptors = np.abs(descriptors) / np.linalg.norm(descriptors)             # normalize over descriptors and disregard phase
     # descriptors = descriptors / np.linalg.norm(descriptors)                     # normalize over descriptors
     # descriptors = descriptors / np.linalg.norm(fft_cont[1:])                    # normalize over the entire fft
-    descriptors = descriptors / np.linalg.norm(descriptors) * np.exp(-1j*np.angle(fft_cont[1]))      # normalize over descriptors and correct for initial phase
-    # descriptors = descriptors / np.linalg.norm(fft_cont[1:]) * np.exp(-1j*np.angle(fft_cont[0]))     # normalize over the entire fft and correct for initial phase
-    # descriptors = descriptors * np.exp(-1j*np.angle(fft_cont[0]))               # correct for initial phase
+    descriptors = descriptors / np.linalg.norm(descriptors) * rotation          # normalize over descriptors and correct for initial phase
+    # descriptors = descriptors / np.linalg.norm(fft_cont[1:]) * rotation         # normalize over the entire fft and correct for initial phase
 
     # step 5 : visualization ?
     if not make_contour :
-        return location, descriptors
+        return location, descriptors, rotation
     else :
         aprx_cont = np.zeros_like(fft_cont)       # make a contour approximation for visualization
         aprx_cont[descr_idx] = fft_cont[descr_idx];
         aprx_cont = np.fft.ifft(aprx_cont)
         aprx_cont = np.array([aprx_cont.real, aprx_cont.imag]).T
-        return location, descriptors, aprx_cont
+        return location, descriptors, aprx_cont, rotation
 
 
 def make_avg_descr(card, nb_descr=10, make_contour=False) :
@@ -64,7 +67,8 @@ def make_avg_descr(card, nb_descr=10, make_contour=False) :
     for n in range(nb_imgs) :
         img = get_img(n, card)
         for contour in get_relevant_contours(img, ll=75) :
-            loc, descriptors = get_fourier_descriptors(contour, nb_descr)
+            loc, descriptors, _ = get_fourier_descriptors(contour, nb_descr)
+            # descriptors = np.abs(descriptors)
             i = int((loc[1]*len(card))//img.shape[1])
             avg_descr[i].append(descriptors)
 
@@ -82,7 +86,7 @@ def make_avg_descr(card, nb_descr=10, make_contour=False) :
             avg_cont[char] = np.array([avg_char_cont.real, avg_char_cont.imag]).T
         return avg_descr, avg_cont
 
-def make_card_space(cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"], nb_descr=10) :
+def make_card_space(cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "C", "D", "H", "S"], nb_descr=10) :
     """
     creates a dictionary containing the centroids of each card value
     """
@@ -97,7 +101,7 @@ def show_card_space(card_space) :
     """
     nb_descr = len(card_space[list(card_space.keys())[0]])
 
-    fig, axes = plt.subplots(3, 5)
+    fig, axes = plt.subplots(4, 5)
     fig.suptitle("card space for nb_descr = {}".format(nb_descr))
 
     for card, ax in zip(card_space.keys(), axes.ravel()[:len(card_space.keys())]) :
@@ -136,14 +140,14 @@ def load_card_space(filename="cardspace.pkl") :
     file.close()
     return card_space
 
-def detect_card_vals(img, card_space, thresh=0.2, min_dist=40, nb_vals=3) :
+def detect_card_symbols(img, card_space, thresh=0.15, min_dist=20, nb_vals=None) :
     """
-    tries to detect the card values of all cards in a given image
+    tries to detect all the card symbols in a given image
     """
     detected_nums = []
     nb_descr = len(card_space[list(card_space.keys())[0]])
     for contour in get_relevant_contours(skimage.filters.unsharp_mask(img, amount=.3, multichannel=True)) :
-        location, descriptors = get_fourier_descriptors(contour, nb_descr)
+        location, descriptors, _ = get_fourier_descriptors(contour, nb_descr)
 
         closest_num = sorted([(np.linalg.norm(np.abs(descriptors) - np.abs(card_space[num])), num) for num in card_space])[0]
         # closest_num = sorted([(np.linalg.norm(descriptors - card_space[num]), num) for num in card_space])[0]
@@ -153,7 +157,8 @@ def detect_card_vals(img, card_space, thresh=0.2, min_dist=40, nb_vals=3) :
 
     # special case : 10 is made up of two characters, which must be fused
     to_remove = set()
-    for i, num1 in enumerate(detected_nums):
+    iterator = enumerate(detected_nums)
+    for i, num1 in iterator:
         if num1[0] == '0' :
             to_remove.add(i)                                                    # remove all 0's
         if num1[0] == '1' :
@@ -161,27 +166,113 @@ def detect_card_vals(img, card_space, thresh=0.2, min_dist=40, nb_vals=3) :
             for j, num2 in enumerate(detected_nums) :
                 if i != j and num2[0] == '0':
                     if np.linalg.norm(num1[1] - num2[1]) < min_dist :
-                        detected_nums.append(('10', (num1[1]+num2[1])/2, 0))   # all 1's associated with 0's are combined to 10's
-    detected_nums = [i for j, i in enumerate(detected_nums) if j not in to_remove]
+                        detected_nums.append(('10', .5*(num1[1]+num2[1]), .5*(num1[2]+num2[2])))   # all 1's associated with 0's are combined to 10's
+    detected_nums = [v for i, v in enumerate(detected_nums) if i not in to_remove]
 
     # remove duplicate or overlapping detections
     to_remove = set()
     for i, num1 in enumerate(detected_nums) :
         for j, num2 in enumerate(detected_nums)  :
-            if i < j and np.linalg.norm(num1[1] - num2[1]) < min_dist :         # compare locations (only one number can be in a single spot)
+            if i == j : break
+            if np.linalg.norm(num1[1] - num2[1]) < min_dist :         # compare locations (only one number can be in a single spot)
                 to_remove.add(i if num1[2] > num2[2] else j)                    # compare cardspace_distance (chose the most likely number)
-    detected_nums = [i for j, i in enumerate(detected_nums) if j not in to_remove]
+    detected_nums = [v for i, v in enumerate(detected_nums) if i not in to_remove]
 
     #only return the most likely detections
     detected_nums = sorted(detected_nums, key=lambda val : val[2])
 
-    return detected_nums[:nb_vals]
+    if nb_vals :
+        return detected_nums[:nb_vals]
+    else :
+        return detected_nums
+
+def guess_cards(img, card_space, expected_cards, disp=False, title='') :
+    """
+    tries to guess what cards are present in an image
+    """
+
+    # container to store detected cards
+    detected_cards = []
+
+    # detect all card-space symbols present in the image
+    card_symbols = detect_card_symbols(img, card_space)
+
+    # sort detected symbols into suits and values
+    suits = [sym for sym in card_symbols if sym[0] in ['C', 'D', 'H', 'S']  ]
+    vals  = [sym for sym in card_symbols if sym[0] not in ['C', 'D', 'H', 'S']  ]
+
+    # associate each value with a suit, if possible
+    to_remove_v, to_remove_s = set(), set()
+    iterator = list(enumerate(vals)) # make iterator before looping
+    for i, v in iterator :
+        ss = [(s, j) for j, s in enumerate(suits) if j not in to_remove_s and (np.linalg.norm(v[1] - s[1]) < 40)]
+        if len(ss) :
+            s, j = min(ss, key=lambda elem: elem[0][2])
+            loc = .5*(v[1] + s[1])
+            lik = .5*(v[2] + s[2])
+            vals.append((v[0] + s[0], loc, lik))
+            to_remove_v.add(i)
+            to_remove_s.add(j)
+    vals  = [v for i, v in enumerate(vals)  if i not in to_remove_v]
+    suits = [s for j, s in enumerate(suits) if j not in to_remove_s]
+
+    # first try to detect the cards that are fully visible
+    to_remove_v, to_remove_s = set(), set()
+    iterator = list(enumerate(vals))
+    for i, v1 in iterator :
+        for j, v2 in iterator :
+            if i == j : break
+            if (i in to_remove_v or j in to_remove_v) : continue
+
+            if np.linalg.norm(v2[1] - v1[1]) < 400 :
+                # spacial case 1 : we've detected a specific card
+                # OR
+                # spacial case 2 : we have a detected a similar value, but no suit
+                # OR
+                # spacial case 3 : confusion between 6 and 9
+                intersect = ''.join([c for c in v1[0] if c in v2[0]])
+                if (intersect == v1[0] and intersect == v2[0]) or               \
+                   (len(intersect) and intersect[-1] not in ['C', 'D', 'H', 'S']):
+                    loc = .5*(v1[1] + v2[1])
+                    if loc[0] < img.shape[0] * 1./3. or loc[0] > img.shape[0] * 2./3. :
+                        break # the card location doesn't make sense
+                    val = intersect
+                    lik = .5*(v1[2] + v2[2])
+                    if val[-1] not in ['C', 'D', 'H', 'S'] :
+                        ss = [(s, k) for k, s in enumerate(suits) if k not in to_remove_s and (np.linalg.norm(v1[1] - s[1]) < 60 or np.linalg.norm(v2[1] - s[1]) < 60)]
+                        s, k = min(ss, key=lambda elem: elem[0][2]) if len(ss) else ('', None)
+                        if s :
+                            to_remove_s.add(k)
+                            val = val + s[0]
+                            lik = .5*(v1[2] + v2[2] + s[2])
+                    to_remove_v.add(i)
+                    to_remove_v.add(j)
+                    detected_cards.append((val, loc, lik))
+    vals  = [v for i, v in enumerate(vals)  if i not in to_remove_v]
+    suits = [s for j, s in enumerate(suits) if j not in to_remove_s]
+    detected_cards = sorted(detected_cards + vals, key=lambda val : val[2])
+    detected_cards = detected_cards[:expected_cards]
 
 
+
+
+
+    if disp :
+        show_img(img, title)
+
+        for card in detected_cards :
+            plt.text(card[1][1]+10, card[1][0]+10, card[0], color='limegreen', size=20)
+
+        for v in vals :
+            plt.text(v[1][1]+10, v[1][0]+10, v[0], color='dodgerblue', size=13)
+        for s in suits :
+            plt.text(s[1][1]+10, s[1][0]+10, s[0], color='deepskyblue', size=12)
+
+    return detected_cards
 
 if __name__ == '__main__':
 
-    nb_descr = 10
+    nb_descr = 20
     # for nb_descr in range(1,11) :
     #     card_space = make_card_space(nb_descr=10*nb_descr)
     #     save_card_space(card_space, f"cardspace_{10*nb_descr}.pkl")
@@ -191,17 +282,24 @@ if __name__ == '__main__':
     # show_card_space(card_space)
     # plt.show()
 
-    exit()
+    card_space = make_card_space(nb_descr=nb_descr)
+    # show_card_space(card_space)
+    # plt.show()
 
-    cards_list = [4, 14, 17, 18] #[0, 2, 3, 4, 5, 7, 8, 9]
-    cards_name = 'cardst'
+    # cards_list = [4, 14, 17, 18] #[0, 2, 3, 4, 5, 7, 8, 9]
+    # cards_name = 'cardst'
+    # for n in cards_list :
+    #     img = get_img(n, cards_name)
+    #
+    #     # print results
+    #     guess_cards(img, card_space, expected_cards=5, disp=True, title="{}_{}".format(cards_name, str(n).zfill(2)))
+    # plt.show()
+
+    cards_list = [0, 2, 3, 4, 5, 7, 8, 9]
+    cards_name = 'cards1'
     for n in cards_list :
         img = get_img(n, cards_name)
-        card_vals = detect_card_vals(img, card_space, thresh=.2, nb_vals=10)
-        print(len(card_vals))
 
         # print results
-        show_img(img, "{}_{}".format(cards_name, str(n).zfill(2)))
-        for val in card_vals :
-            plt.text(val[1][1]+10, val[1][0]+10, val[0], color='g', size=20)
+        guess_cards(img, card_space, expected_cards=2, disp=True, title="{}_{}".format(cards_name, str(n).zfill(2)))
     plt.show()
